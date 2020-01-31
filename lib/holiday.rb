017c81ecd20dd619b8407fb2821ef424bdc54908
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-31 18:10:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,7 +23,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |holiday|
+  supply = "Balloons"
+  holiday_hash[:winter].each do |holiday, supply|
     holiday << supply
   end
 
