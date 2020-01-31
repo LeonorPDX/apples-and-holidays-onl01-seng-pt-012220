5680529c4f7c9a5ca0938816284d43217766abf6
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-31 18:19:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   supply = "Grill"
-  holiday_hash[:summer][:memorial_day] 
+  holiday_hash[:spring][:memorial_day] << supply 
   
 end
 
