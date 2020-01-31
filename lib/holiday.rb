8d12da627a30b83733d65343bca7bd4edebab383
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-31 18:37:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,10 +61,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, holiday_name|
+  holiday_hash.each do |season, holiday_named_hash|
     puts "#{season.capitalize}:"
-      holiday_name.each do |name, supplies|
-        puts "#{name}: #{supplies}"
+      holiday_named_hash.each do |name, supplies|
+        puts name
+        puts supplies
       end
   end
 
